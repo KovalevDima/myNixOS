351cdaa906b211f69de53a1979eff303fad9d1d0
Kovalev Dmitry <onarainydayillwinanyway@gmail.com>
2024-09-27 23:47:41 +0300

    Add tree-sitter for neovim

--- home/editor.nix
+++ home/editor.nix
@@ -12,6 +12,18 @@
   config = lib.mkIf config.module.editor.enable {
     programs.neovim = {
       enable = true;
+      plugins = with pkgs.vimPlugins; [
+        telescope-nvim
+        (nvim-treesitter.withPlugins
+          (p : [
+            p.tree-sitter-nix
+            p.tree-sitter-bash
+            p.tree-sitter-json
+            p.tree-sitter-yaml
+            p.tree-sitter-markdown
+          ])
+        )
+      ];
     };
     programs.vscode = {
       enable = true;
