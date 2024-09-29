11f7d23e5785fedc40e3b0d5f790f9c518a08137
Kovalev Dmitry <onarainydayillwinanyway@gmail.com>
2024-09-30 01:24:46 +0300

    Rename host

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     ];
   in {
     nixosConfigurations = {
-      nixos = inputs.nixpkgs.lib.nixosSystem {
+      laptop = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
