adcbed92172e164636776df811c63ad56d7b5f69
Kovalev Dmitry <onarainydayillwinanyway@gmail.com>
2025-01-19 18:43:19 +0300

    Add neovim config

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -29,6 +29,72 @@ in
           config = "colorscheme nix-${config.colorScheme.slug}";
         }
       ];
+
+      extraConfig = ''
+        " Tabs/spaces
+        set tabstop=2
+        set expandtab
+        set shiftwidth=2
+
+        " Navigation
+        set scrolloff=3             " some lines around scroll for context
+
+        " Cursor/Line
+        set number
+        set relativenumber
+        set colorcolumn=-0          " based on textwidth
+        set cursorline              " highlight the current line
+
+        " Status/History
+        set history=200             " remember a lot of stuff
+        set ruler                   " Always show info along bottom.
+        set cmdheight=1
+
+        " Scrolling
+        set ttyfast
+
+        " Files
+        set autoread                            " auto-reload files changed on disk
+        set updatecount=0                       " disable swap files
+        set wildmode=longest,list,full 
+
+        " Vimdiff
+        set diffopt=filler,vertical
+
+        " Conceal (disabled by default)
+        set conceallevel=0
+
+        " Wrapping
+        set nowrap
+
+        " Leader
+        nnoremap <Space> <Nop>
+        let mapleader = ' '
+        let maplocalleader = ' '
+
+        " Make F1 work like Escape.
+        map <F1> <Esc>
+        imap <F1> <Esc>
+
+        " Mouse issue (https://github.com/neovim/neovim/wiki/Following-HEAD#20170403)
+        set mouse=a
+
+        " Use system clipboard for yanks.
+        set clipboard+=unnamedplus
+
+        " Use ,t for 'jump to tag'.
+        nnoremap <Leader>t <C-]>
+
+        " Allow hidden windows
+        set hidden
+
+        " Grep with rg
+        set grepprg=rg\ --line-number\ --column
+        set grepformat=%f:%l:%c:%m
+
+        " Theme
+        set termguicolors
+      '';
     };
   };
 }
