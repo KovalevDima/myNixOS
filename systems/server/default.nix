cff7607a44689d64964e8f1550b17028c3585ab9
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-02-27 23:39:43 +0300

    Update ClickHaskell

--- systems/server/default.nix
+++ systems/server/default.nix
@@ -44,6 +44,8 @@
               locations."/" = {
                 proxyPass = "http://unix:${config.ClickHaskell.path}/ClickHaskell.sock:";
                 extraConfig = ''
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection "Upgrade";
                   proxy_set_header Host $host;
                   proxy_set_header X-Real-IP $remote_addr;
                   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
