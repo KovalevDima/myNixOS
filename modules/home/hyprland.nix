4ecc5a4fa6283982770e62794e7a907dc1b6686f
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-05-30 07:23:24 +0300

    Hyprland: Disable ANR dialog

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -116,6 +116,7 @@ in
         misc = {
           force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
           disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+          enable_anr_dialog = false;
         };
 
         # https://wiki.hyprland.org/Configuring/Variables/#input
