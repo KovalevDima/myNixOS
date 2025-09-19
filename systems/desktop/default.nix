c7654746e3d3aa415948c4651ba151305696ba57
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-09-19 17:11:53 +0300

    Add overlay for amnezia

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -20,6 +20,25 @@
           };
         };
         module.gui.initialUser = "dmitry";
+        nixpkgs.overlays = [
+          (final: prev: {
+            amneziawg-go = prev.amneziawg-go.overrideAttrs (
+              finalAttrs: prevAttrs: {
+                name = "amneziawg-go";
+                version = "0.2.13";
+
+                src = prev.fetchFromGitHub {
+                  owner = "amnezia-vpn";
+                  repo = "amneziawg-go";
+                  tag = "v${finalAttrs.version}";
+                  hash = "sha256-vXSPUGBMP37kXJ4Zn5TDLAzG8N+yO/IIj9nSKrZ+sFA=";
+                };
+
+                vendorHash = "sha256-9OtIb3UQXpAA0OzPhDIdb9lXZQHHiYCcmjHAU+vCtpk=";
+              }
+            );
+          })
+        ];
         programs = {
           amnezia-vpn.enable = true;
           steam = {
